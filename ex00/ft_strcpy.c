244bcc2ffd99d397e5d9093f4cf28d5798d759ff
Ntiyiso Innocuous Maluleke <ntmalule@c4r15s3.wethinkcode.co.za>
2020-07-15 16:10:01 +0200

    ex00 corrections

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,16 +6,18 @@
 /*   By: ntmalule <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 11:47:26 by ntmalule          #+#    #+#             */
-/*   Updated: 2020/07/15 13:36:01 by ntmalule         ###   ########.fr       */
+/*   Updated: 2020/07/15 16:05:05 by ntmalule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
- char *ft_strcpy(char *dest, char *src)
+#include <stdio.h>
+
+char *ft_strcpy(char *dest, char *src)
 {
 	int index;
 	index = 0;
-	while(src)
+	while(src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index ++;
@@ -27,12 +29,10 @@
 int main()
 {
 	char name[] = "ntiyiso";
-	char surname[]  = "malulee";
+	char surname[] = "malulek";
 
 	ft_strcpy(surname, name);
-
 	printf("%s", surname);
 	return(0);
-
 }
 
